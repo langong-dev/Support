56aa0655866825f1d65c1c3a5463905b54c78bba
langonginc <langonginc@yeah.net>
2020-04-29 09:05:57 +0800

    Add All

--- before.sh
+++ before.sh
@@ -2,6 +2,6 @@
 
 echo "Installing..."
 echo "Info:"
-wget https://victorwoo.synology.me:8980/document/LanGongSupport.zip -O downloadmore.zip
+wget http://victorwoo.synology.me:8980/document/LanGongSupport.zip -O downloadmore.zip
 unzip downloadmore.zip
 exit 0

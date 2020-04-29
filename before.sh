a99043ac323c58773b076f9ed4caff4c4e74ede5
langonginc <langonginc@yeah.net>
2020-04-29 10:31:25 +0800

    Add All

--- before.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing..."
-echo "Info:"
-wget http://victorwoo.synology.me:8980/document/LanGongSupport.zip -O downloadmore.zip
-unzip downloadmore.zip
-exit 0
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /tmp/LanGong && npm start 
